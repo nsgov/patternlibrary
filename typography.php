a3a9bbe8e8e6614dfd067127cf0602d8950b0f9d
Kayte <kayte@kayte.ca>
2016-08-26 11:59:06 -0300

    Many additions and tweaks.
    Adjustments to icon-list class, minor typographic adjustments.
    Additions and reconfiguration of patternlab categories.

--- typography.php
+++ typography.php
@@ -147,42 +147,11 @@ foreach ($feedbacks as $feedback):
     echo '<textarea rows="6" cols="30">';
     echo htmlspecialchars(file_get_contents('feedback/'.$feedback));
     echo '</textarea>';
-    echo '<p><a href="feedback/'.$feedback.'">'.$feedback.'</a></p>';
+    echo '<p><a href="feedbacks/'.$feedback.'">'.$feedback.'</a></p>';
     echo '</div>';
     echo '</div>';
 endforeach;
 ?>
-
-
-
-<?php
-$contents = array();
-$handle=opendir('content');
-while (false !== ($content = readdir($handle))):
-    if(stristr($content,'.html')):
-        $contents[] = $content;
-    endif;
-endwhile;
-sort($contents);
-
-?>
-<h2 class="section">Content</h2>
-<?php
-foreach ($contents as $content):
-    echo '<div class="pattern">';
-    echo '<div class="display">';
-    include('content/'.$content);
-    echo '</div>';
-    echo '<div class="source">';
-    echo '<textarea rows="6" cols="30">';
-    echo htmlspecialchars(file_get_contents('content/'.$content));
-    echo '</textarea>';
-    echo '<p><a href="content/'.$content.'">'.$content.'</a></p>';
-    echo '</div>';
-    echo '</div>';
-endforeach;
-?>
-
 
 
 
