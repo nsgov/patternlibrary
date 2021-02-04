a4d3373112babab0249ca836e5213c2f6a3e262d
David Nordlund <nordlund@users.noreply.github.com>
2021-02-04 15:12:25 -0400

    Change build script to build *.php instead of a hardcoded list.

--- bin/build-pages.sh
+++ bin/build-pages.sh
@@ -1,24 +1,15 @@
-#!/bin/sh
+#!/bin/bash -e
 
+cd "`dirname "$0"`/.."
+
+[ -d .public ] && rm -fr .public
 mkdir .public
-php index.php > .public/index.html
-php accordion.php > .public/accordion.html
-php animation.php > .public/animation.html
-php buttons.php > .public/buttons.html
-php content.php > .public/content.html
-php coronavirus.php > .public/coronavirus.html
-php forms.php > .public/forms.html
-php grid-system.php > .public/grid-system.html
-php icons.php > .public/icons.html
-php lists.php > .public/lists.html
-php logos.php > .public/logos.html
-php messages.php > .public/messages.html
-php metadata.php > .public/metadata.html
-php navigation.php > .public/navigation.html
-php pages.php > .public/pages.html
-php tables.php > .public/tables.html
-php typography.php > .public/typography.html
-php widgets.php > .public/widgets.html
-php videos.php > .public/videos.html
+
+for php_file in *.php; do
+	file="`basename "$php_file" .php`" # filename w/o extension
+	echo "php $file.php > public/$file.html"
+	php "$file.php" > ".public/$file.html"
+done
+
 cp -r * .public
 mv .public public
