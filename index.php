ce03a562de03e351253665fc52d3f7071aa1fe13
Kayte McLaughlin <kayte@kayte.ca>
2021-02-05 13:54:58 -0400

    Add 'links' to the list on index, class change on colour pg

--- index.php
+++ index.php
@@ -16,22 +16,6 @@
   <img src="img/ns-vip-white.svg" alt="Government of Nova Scotia" style="margin: 2em; width: 200px; height: auto;" />
 </div>
 
-<div class="pattern-container">
-<h1>Government of Nova Scotia UI Pattern Library</h1>
-  
-<div class="row">
-  <div class="col-xs-12 col-sm-4 content-chunk">
-    <h2><a href="#">Application and service patterns</a></h2>
-  </div>
-    
-  <div class="col-xs-12 col-sm-4 content-chunk">
-    <h2><a href="#">Core web content patterns</a></h2>
-  </div>
-    
-  <div class="col-xs-12 col-sm-4 content-chunk">
-    <h2><a href="#">Marketing content patterns</a></h2>
-  </div>
-</div>
 
 <p>This is a living collection that will continue to evolve and expand. If you can't find what you are looking for, email <a href="internet@novascotia.ca">internet@novascotia.ca</a> and we will work with you to help you find a solution.</p>
 
@@ -46,9 +30,11 @@
   <ul class="ui-categories">
     <!-- <li><a href="accordion.html">Accessible accordion</a></li> -->
     <li><a href="buttons.html">Buttons</a></li>
+    <!-- <li><a href="colours.html">Colours</a></li> -->
     <li><a href="content.html">Content</a></li>
     <li><a href="forms.html">Form elements</a></li>
     <li><a href="icons.html">Icons</a></li>
+    <li><a href="links.html">Links</a></li>
     <li><a href="lists.html">Lists</a></li>
     <!-- <li><a href="logos.html">Logos</a></li> -->
     <li><a href="messages.html">Messages/Feedback</a></li>
