f71ad11571f805f3941167cbbf4d8fa67faadc03
hypnokayte <kayte@kayte.ca>
2021-02-12 15:02:24 +0000

    Deploying to github-pages from  @ ff498b7caceb4010936b575e05c55891418491db 🚀

--- links.php
+++ links.php
@@ -1,37 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Saltire Pattern Library</title>
-<link rel="stylesheet" href="global.css">
-<link rel="stylesheet" href="https://beta.novascotia.ca/themes/ignition/saltire.css">
-<link rel="stylesheet" href="https://novascotia.ca/clf/fonts/font-awesome/css/font-awesome.min.css">
-
-</head>
-<body>
-	
-<!-- NS MOTHERSHIP RIBBON -->
-<nav id="navigation" class="ribbon" role="navigation" aria-label="Main navigation">
-	<div class="container">
-		<div class="row">
-			<div class="col-xs-12">
-				<a href="https://novascotia.ca/" hreflang="en"><img class="ribbon-img" src="https://novascotia.ca/clf/ignition/img/nsvip-en-white-flag.svg" alt="Government of Nova Scotia" /></a>
-			</div>
-		</div>
-	</div>
-</nav>	
-<!-- NS MOTHERSHIP RIBBON -->
-
-
-<div class="container">
-	<div class="row">
-		<div class="col-xs-12">
-
-			<span aria-hidden="true" id="you-are-here-text">you are here</span>
-			<nav class="breadcrumbs" id="breadcrumbs">
-				<a href="https://cns.pages.novascotia.ca/patternlab/" rel="up" class="breadcrumb" hreflang="en" lang="en">Government of Nova Scotia UI Pattern Library</a>
-				<span class="breadcrumb">Links</span>
-			</nav>
+<?php
+	$title = 'Links';
+	include 'inc/header.php';
+	include 'inc/breadcrumb.php';
+?>
 
 <h1>Links</h1>
 
@@ -255,8 +226,6 @@
 </p>
 
 
-		</div>
-	</div>
-</div>
-</body>
-</html>
+<?php
+	include 'inc/footer.php';
+?>
