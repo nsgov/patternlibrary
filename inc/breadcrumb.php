b9ec9ee2d47609c370eb0a2c50f90e5ecae014e9
Kayte McLaughlin <kayte@kayte.ca>
2021-03-01 13:12:06 -0400

    Added conditional statement to check host and return correct path

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -1,6 +1,15 @@
-
+<?php
+$host = $_SERVER['HTTP_HOST']; 
+	if($host == "nsgov.github.io" or $host == "github.io") {
+		echo 'https://nsgov.github.io/patternlibrary'; 
+	} else {
+		echo 'https://cns.pages.novascotia.ca/patternlab/';
+	} 
+?>
+		
+			
 			<span aria-hidden="true" id="you-are-here-text">you are here</span>
 			<nav class="breadcrumbs" id="breadcrumbs">
-				<a href="https://cns.pages.novascotia.ca/patternlab/" rel="up" class="breadcrumb" hreflang="en" lang="en">Government of Nova Scotia UI Pattern Library</a>
+				<a href="<?php echo "$host"; ?>" rel="up" class="breadcrumb" hreflang="en" lang="en">Government of Nova Scotia UI Pattern Library</a>
 				<span class="breadcrumb"><?php echo "$title"; ?></span>
 			</nav>
