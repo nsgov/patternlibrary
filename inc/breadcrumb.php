7c2e90d08f3848f32f32af1234f9fe657969984e
hypnokayte <kayte@kayte.ca>
2021-03-01 17:29:57 +0000

    Deploying to github-pages from  @ a2af79df9119b1f0bdfb2b53c3216e441c1f02c0 🚀

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -1,15 +1,14 @@
-<?php
-$host = $_SERVER['HTTP_HOST']; 
-	if($host == "nsgov.github.io" or $host == "github.io") {
-		echo 'https://nsgov.github.io/patternlibrary'; 
-	} else {
-		echo 'https://cns.pages.novascotia.ca/patternlab/';
-	} 
-?>
+<!--
+	<?php
+
+		echo $_SERVER['HTTP_HOST'];
+
+	?>
+-->
 		
 			
 			<span aria-hidden="true" id="you-are-here-text">you are here</span>
 			<nav class="breadcrumbs" id="breadcrumbs">
-				<a href="<?php echo "$host"; ?>" rel="up" class="breadcrumb" hreflang="en" lang="en">Government of Nova Scotia UI Pattern Library</a>
+				<a href="../" rel="up" class="breadcrumb" hreflang="en" lang="en">Government of Nova Scotia UI Pattern Library</a>
 				<span class="breadcrumb"><?php echo "$title"; ?></span>
 			</nav>
