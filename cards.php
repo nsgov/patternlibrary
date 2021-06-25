1224277414385be34d0d91362d22e191f38fc423
Kayte McLaughlin <kayte@kayte.ca>
2021-06-25 10:46:55 -0300

    Add PHP foreach back into cards, extended-grid, and tags.=

--- cards.php
+++ cards.php
@@ -10,105 +10,36 @@
 	Cards are used to organise information, providing a brief summary of content or a task linked to more detail. They can be used to display feature stories, or groups of tasks.
 </p>
 
-<div class="pattern"> 
 
-<section class="card-grid feature-story">
-	<article class="card">
-		<img src="img/spring-garden.jpg" alt="feature story img" />
+<?php
+$cards = array();
+$handle=opendir('cards');
+while (false !== ($card = readdir($handle))):
+    if(stristr($card,'.html')):
+        $cards[] = $card;
+    endif;
+endwhile;
+sort($cards);
 
-		<div class="card-body">
-			<h2><a href="#">Linked title</a></h2>
-			<p>Card with rounded corners.</p>
-	</article>
-	<article class="card">
-		<img src="img/spring-garden.jpg" alt="feature story img" />
+?>
+<h2 class="section">Card patterns</h1>
+<?php
+foreach ($cards as $card):
+    echo '<div class="pattern">';
+    echo '<div class="display">';
+    include('cards/'.$card);
+    echo '</div>';
+    echo '<div class="source">';
+    echo '<textarea rows="6" cols="30">';
+    echo htmlspecialchars(file_get_contents('cards/'.$card));
+    echo '</textarea>';
+    echo '<p><a href="cards/'.$card.'">'.$card.'</a></p>';
+    echo '</div>';
+    echo '</div>';
+    echo '<div class="pattern-separator"></div>';
 
-		<div class="card-body">
-			<h2><a href="#">Linked title</a></h2>
-			<p>Card with rounded corners.</p>
-	</article>
-	<article class="card">
-		<img src="img/spring-garden.jpg" alt="feature story img" />
-
-		<div class="card-body">
-			<h2><a href="#">Linked title</a></h2>
-			<p>Card with rounded corners.</p>
-	</article>
-</section>
-
-
-</div>
-<div class="pattern">
-<textarea rows="20" style="width: 100%">
-
-<section class="card-grid feature-story">
-	<article class="card">
-		<img src="img/spring-garden.jpg" alt="feature story img" />
-
-		<div class="card-body">
-			<h2><a href="#">Linked title</a></h2>
-			<p>Card with rounded corners.</p>
-	</article>
-	<article class="card">
-		<img src="img/spring-garden.jpg" alt="feature story img" />
-
-		<div class="card-body">
-			<h2><a href="#">Linked title</a></h2>
-			<p>Card with rounded corners.</p>
-	</article>
-	<article class="card">
-		<img src="img/spring-garden.jpg" alt="feature story img" />
-
-		<div class="card-body">
-			<h2><a href="#">Linked title</a></h2>
-			<p>Card with rounded corners.</p>
-	</article>
-</section>
-
-
-</textarea>
-<p><a href="cards/card-feature-story.html">card-feature-story.html</a></p>
-</div>
-
-
-
-<div class="pattern">
-
-<section class="card-grid tint">
-	<article class="card">
-		<img src="img/spring-garden.jpg" alt="feature story img" />
-
-		<div class="card-body">
-			<h2><a href="#">Linked title</a></h2>
-			<p>Simple card with blue tint background.</p>
-	</article>
-	<article class="card">
-		<img src="img/spring-garden.jpg" alt="feature story img" />
-
-		<div class="card-body">
-			<h2><a href="#">Linked title</a></h2>
-			<p>Simple card with blue tint background.</p>
-	</article>
-	<article class="card">
-		<img src="img/spring-garden.jpg" alt="feature story img" />
-
-		<div class="card-body">
-			<h2><a href="#">Linked title</a></h2>
-			<p>Simple card with blue tint background.</p>
-	</article>
-</section>
-
-
-</div>
-<div class="pattern">
-<textarea rows="20" style="width: 100%">
-
-</textarea>
-<p><a href="cards/card-tint.html">card-tint.html</a></p>
-</div>
-
-
-
+endforeach;
+?>
 
 <?php
 include 'inc/footer.php';
--- extended-grid.php
+++ extended-grid.php
@@ -10,135 +10,35 @@
 	Some provisional grid patterns leveraging CSS grid and flexbox.
 </p>
 
+<?php
+$extendedgrids = array();
+$handle=opendir('extendedgrid');
+while (false !== ($extendedgrid = readdir($handle))):
+    if(stristr($extendedgrid,'.html')):
+        $extendedgrids[] = $extendedgrid;
+    endif;
+endwhile;
+sort($extendedgrids);
 
-<div class="pattern">
-	<div class="display">
-<!-- Columns: Column 2 automatically fills up remaining width -->
-<div class="row flex">
-	<div class="col">
-		<div>
-			<img src="img/spring-garden.jpg" style="width: 100px; height: auto;" />
-		</div>
-	</div>
-	<div class="col flex-1">
-		<div>
-			<p>
-				Adding the <span class="code">flex-1</span> class to this column makes it fill the remaining width.
-			</p>
-		</div>
-	</div>
-</div>
+?>
+<h2 class="section">Card patterns</h1>
+<?php
+foreach ($extendedgrids as $extendedgrid):
+    echo '<div class="pattern">';
+    echo '<div class="display">';
+    include('extendedgrid/'.$extendedgrid);
+    echo '</div>';
+    echo '<div class="source">';
+    echo '<textarea rows="6" cols="30">';
+    echo htmlspecialchars(file_get_contents('extendedgrid/'.$extendedgrid));
+    echo '</textarea>';
+    echo '<p><a href="extendedgrid/'.$extendedgrid.'">'.$extendedgrid.'</a></p>';
+    echo '</div>';
+    echo '</div>';
+    echo '<div class="pattern-separator"></div>';
 
-
-	 
-
-	</div>
-	<div class="source">
-		<textarea rows="6" cols="30">
-		
-	<!-- Columns: Column 2 automatically fills up remaining width -->
-<div class="row flex">
-	<div class="col">
-		<div>
-			<img src="img/spring-garden.jpg" style="width: 100px; height: auto;" />
-		</div>
-	</div>
-	<div class="col flex-1">
-		<div>
-			<p>
-				Adding the <span class="code">flex-1</span> class to this column makes it fill the remaining width.
-			</p>
-		</div>
-	</div>
-</div>
-
-
-		</textarea>
-	<p><a href="extendedgrid/row-cols-column-2-auto-fill.html">row-cols-column-2-auto-fill.html</a></p>
-	</div>
-</div>
-
-
-
-<div class="pattern">
-	<div class="display">
-
-	<div class="row flex flex-wrap">
-	<div class="col col-12 col-6-sm col-4-mid col-3-lg">
-		<div class="gray-bg">Column 1</div>
-	</div>
-	<div class="col col-12 col-6-sm col-4-mid col-3-lg">
-		<div class="gray-bg">Column 2</div>
-	</div>
-	<div class="col col-12 col-6-sm col-4-mid col-3-lg">
-		<div class="gray-bg">Column 3</div>
-	</div>
-	<div class="col col-12 col-6-sm col-4-mid col-3-lg">
-		<div class="gray-bg">Column 4</div>
-	</div>
-</div>
-
-	</div>
-	<div class="source">
-		<textarea rows="6" cols="30">
-<div class="row flex flex-wrap">
-	<div class="col col-12 col-6-sm col-4-mid col-3-lg">
-		<div class="gray-bg">Column 1</div>
-	</div>
-	<div class="col col-12 col-6-sm col-4-mid col-3-lg">
-		<div class="gray-bg">Column 2</div>
-	</div>
-	<div class="col col-12 col-6-sm col-4-mid col-3-lg">
-		<div class="gray-bg">Column 3</div>
-	</div>
-	<div class="col col-12 col-6-sm col-4-mid col-3-lg">
-		<div class="gray-bg">Column 4</div>
-	</div>
-</div>
-	
-		</textarea>
-	<p><a href="extendedgrid/different-columns-at-breakpoints.html">different-columns-at-breakpoints.html</a></p>
-	</div>
-</div>
-
-
-
-
-<div class="pattern">
-	<div class="display">
-
-	<div class="row flex flex-align-center">
-	<div class="col">
-		<div class="gray-bg">Column 1</div>
-	</div>
-	<div class="col">
-		<div class="gray-bg">
-		<p>Column 2: Adding <span class="code">flex-align-centre</span> to the parent row will vertically align the columns to the centre. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consectetur augue id est accumsan, ut ornare lectus auctor. Aliquam laoreet ante vitae lorem rutrum, quis faucibus nunc elementum. Nulla ornare turpis a nulla pretium ultrices. Sed ultricies venenatis enim a condimentum. Suspendisse sed augue vulputate, rhoncus lorem id, blandit ante. Sed ut nulla vel risus fermentum vehicula semper eu est. Quisque vel odio feugiat, tincidunt nunc eget, mollis dolor. Curabitur eros risus, tincidunt ut ante ac, condimentum consectetur ligula. Mauris euismod sem id est eleifend condimentum. Vestibulum malesuada luctus est et fringilla. Duis laoreet venenatis condimentum. Fusce auctor ligula et erat varius, nec pulvinar augue posuere. Pellentesque mi sapien, lacinia in risus a, iaculis rhoncus mi. Proin non mauris vitae urna ullamcorper cursus. 
-		</p>
-		</div>
-	</div>
-</div>
-
-	</div>
-	<div class="source">
-		<textarea rows="6" cols="30">
-<div class="row flex flex-align-center">
-	<div class="col">
-		<div class="gray-bg">Column 1</div>
-	</div>
-	<div class="col">
-		<div class="gray-bg">
-		<p>Column 2: Adding <span class="code">flex-align-centre</span> to the parent row will vertically align the columns to the centre. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consectetur augue id est accumsan, ut ornare lectus auctor. Aliquam laoreet ante vitae lorem rutrum, quis faucibus nunc elementum. Nulla ornare turpis a nulla pretium ultrices. Sed ultricies venenatis enim a condimentum. Suspendisse sed augue vulputate, rhoncus lorem id, blandit ante. Sed ut nulla vel risus fermentum vehicula semper eu est. Quisque vel odio feugiat, tincidunt nunc eget, mollis dolor. Curabitur eros risus, tincidunt ut ante ac, condimentum consectetur ligula. Mauris euismod sem id est eleifend condimentum. Vestibulum malesuada luctus est et fringilla. Duis laoreet venenatis condimentum. Fusce auctor ligula et erat varius, nec pulvinar augue posuere. Pellentesque mi sapien, lacinia in risus a, iaculis rhoncus mi. Proin non mauris vitae urna ullamcorper cursus. 
-		</p>
-		</div>
-	</div>
-</div>
-	
-		</textarea>
-	<p><a href="extendedgrid/vertically-centred-columns.html">vertically-centred-columns.html</a></p>
-	</div>
-</div>
-
+endforeach;
+?>
 
 
 <?php
