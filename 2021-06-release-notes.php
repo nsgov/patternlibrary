cd192096205a8c669ead9014c8810474bf1b7311
Kayte McLaughlin <kayte@kayte.ca>
2021-06-25 14:16:28 -0300

    Typography, Forms advice, release notes

--- 2021-06-release-notes.php
+++ 2021-06-release-notes.php
@@ -7,23 +7,27 @@
 <h1>June 2021 release notes</h1>
 
 <p>
-	Updated patterns<sup><a href="#footnote">*</a></sup>:
+	Updates:
 </p>
 <ul>
-	<li>Breadcrumb: rewritten to support <span class="code">&lt;ol&gt;</span></li>
+	<li>Breadcrumb: rewritten to use <span class="code">&lt;ol&gt;</span></li>
 	<li>Messages: rewritten to use <span class="code">&lt;aside&gt;</span> element</li>
 	<li>Ribbon header: size variations</li>
+	<li>Typography: removed old patterns, added details about CSS font stacks and font sizes.</li>
 </ul>
 
-<p>Additions:</p>
+<p>
+	Additions:
+</p>
 <ul>
 	<li>Card patterns</li>
 	<li>Colour detail page</li>
+	<li>Extended grid patterns</li>
 	<li>Dates and numbers guidance</li>
-	<li>Ribbon header size variations (xs-lg)</li>
 	<li>Ribbon footer</li>
 	<li>Tags</li>
 </ul>
+
 
 <p id="footnote">
 	* Legacy patterns will still work.
