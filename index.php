c0e9aeac39beddcde5134f8ea1e2bbc2484cf857
Kayte McLaughlin <kayte@kayte.ca>
2021-07-05 16:24:26 -0300

    Updates - follow up on June 25 release

--- index.php
+++ index.php
@@ -1,46 +1,164 @@
-<?php
-	$title = 'Government of Nova Scotia UI Pattern Library';
-	include 'inc/header.php';
-?>
-
-<h1><?php echo "$title"; ?></h1>
-
-<p>This is a living collection that will continue to evolve and expand. If you can't find what you are looking for, email <a href="internet@novascotia.ca">internet@novascotia.ca</a> and we will work with you to help you find a solution.</p>
-
-<p>You can look up different elements and grab the HTML you need to use them.</p>
-
-<p>Please refer to our <a href="https://novascotia.sharepoint.com/sites/Projects/CNSGovWebStrategy/Tools%20and%20Guides/Forms/AllItems.aspx">tools and guides</a> for guidance on content criteria, online writing, and other topics.</p>
-
-<p>You can <a href="css/saltire.css">grab the latest version of the Saltire CSS file</a>.
+<!DOCTYPE html>
+<html lang="en" class="js">
+<head>
+<meta charset="utf-8">
+<title>Government of Nova Scotia - Saltire Pattern Library</title>
+<link rel="stylesheet" href="css/saltire.css">
+<link rel="stylesheet" href="https://novascotia.ca/clf/fonts/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/aria.accordion.css">
+<link rel="stylesheet" href="css/step-by-step.css">
+<link rel="stylesheet" href="global.css">
 
 
-<h2>Categories</h2>
-  <ul class="ui-categories">
-    <!-- <li><a href="accordion.html">Accessible accordion</a></li> -->
-    <li><a href="buttons.html">Buttons</a></li>
-    <!-- <li><a href="colours.html">Colours</a></li> -->
-    <li><a href="content.html">Content</a></li>
-    <li><a href="forms.html">Form elements</a></li>
-    <li><a href="icons.html">Icons</a></li>
-    <li><a href="links.html">Links</a></li>
-    <li><a href="lists.html">Lists</a></li>
-    <!-- <li><a href="logos.html">Logos</a></li> -->
-    <li><a href="messages.html">Messages/Feedback</a></li>
-    <li><a href="navigation.html">Navigation and structural elements</a></li>
-    <li><a href="pages.html">Pages</a></li>
-    <li><a href="grid-system.html">Responsive grid system</a></li>
-    <li><a href="tables.html">Tables</a></li>
-    <li><a href="typography.html">Typography</a></li>
-    <li><a href="videos.html">Videos</a></li>
-  </ul>
+<style>
 
-<h2>Other information</h2>
-  <ul class="ui-categories">
-    <li><a href="metadata.html">How we use metadata</a></li>
-    <li><a href="coronavirus.html">Experimental patterns developed during the Coronavirus pandemic</a></li>
-    <!-- <li><a href="animation.html">Loading animation</a></li> -->
-  </ul>
-  
+		figure {
+			background: #efefef;
+			padding: 4px;
+			margin: auto auto 1.5em auto;
+			text-align: center;
+			font-size: small;
+		}
+
+		.hero-headline-container img {top: -200px;}
+		.hero-headline-container .hero-graphics {left: -38%;}
+		.hero-headline-container h1 {font-size: 2.75em; max-width: 28em;}
+		@media only screen and (max-width: 1024px) {
+			.hero-headline-container h1 {font-size: 2.5em;}
+		}
+		@media only screen and (max-width: 760px) {
+			.hero-headline-container img {top: 0px;}
+		}
+.content-chunk {background: #d4e7f0; border-radius: 6px; padding: 1.5em; margin-bottom: 1.5em; color: #333; z-index: 2; position: relative;}
+		.card-grid {
+			display: grid;
+			grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+			grid-template-rows: auto;
+			grid-gap: 2rem;
+		}
+
+		.card-grid.tint .card {
+			background: #efefef;
+		}
+
+</style>
+
+</head>
+<body>
+
+<header>
+<!-- NS MOTHERSHIP RIBBON -->
+<nav id="navigation" class="ribbon-md" role="navigation" aria-label="Main navigation">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<a href="https://novascotia.ca/" hreflang="en"><img class="ribbon-img" src="https://novascotia.ca/clf/ignition/img/nsvip-en-white-flag.svg" alt="Government of Nova Scotia" /></a>
+			</div>
+		</div>
+	</div>
+</nav>	
+<!-- NS MOTHERSHIP RIBBON -->
+</header>
+
+
+<figure id="hero-headline-container" class="hero-headline-container" style=";">
+		<img src="img/AppleBlossom_LWaggjpg.jpg" alt="Apple blossom"/>
+		<div class="hero-angles"></div>
+	
+		<figcaption id="hero-headline" class="hero-headline">
+			<div class="hero-graphics"><div class="hero-background"></div></div>
+	
+			<h1>Government of Nova Scotia Pattern Library</h1>
+	
+		</figcaption>
+	</figure>
+
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+
+
+
+      <h2>How to contribute</h2>
+        <p>Create an issue on the <a href="https://gitlab.novascotia.ca/cns/patternlab">GitLab project</a> to report an issue or request a feature.</p>
+        <p>This is a living collection that will continue to evolve and expand. If you can't find what you are looking for, email <a href="internet@novascotia.ca">internet@novascotia.ca</a> and we will work with you to help you find a solution.</p>
+
+<section class="card-grid tint rounded">
+    <article class="card">
+      <div class="card-body">
+        <h2>Components</h2>
+        <p>
+        Design patterns used to build Government websites and services and the code you need to use them.
+        </p>
+        <ul>
+          <li><a href="buttons.html">Buttons</a></li>
+          <li><a href="content.html">Content</a></li>
+          <li><a href="icons.html">Icons</a></li>
+          <li><a href="links.html">Links</a></li>
+          <li><a href="lists.html">Lists</a></li>
+          <li><a href="messages.html">Messages and alerts</a></li>
+          <li><a href="navigation.html">Navigation and structural elements</a></li>
+          <li><a href="grid-system.html">Responsive grid system</a></li>
+          <li><a href="tables.html">Tables</a></li>
+          <li><a href="tags.html">Tags</a></li>
+          <li><a href="videos.html">Video</a></li>
+        </ul>
+        </div>
+    </article>
+    <article class="card">
+      <div class="card-body">
+        <h2>Latest updates</h2>
+        <p>
+        <a href="2021-06-release-notes.html">June 2021 release notes <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
+
+        </p>
+       
+        <p>Provisional patterns that are in progress, under review or still being tested:</p>
+        <ul>
+          <li><a href="cards.html">Cards</a></li>
+          <li><a href="coronavirus.html">Coronavirus response patterns</a></li>
+          <li><a href="date-number-formats.html">Date and number formats</a></li>
+          <li><a href="extended-grid.html">Extended grid patterns</a></li>
+      </div>
+    </article>
+    <article class="card">
+      <div class="card-body">
+        <h2>Guidelines</h2>
+        
+        <ul>
+            <li><a href="docs/Content-Criteria-Guide-v2.pdf">Content Criteria Guide (PDF)</a></li>
+            <!-- <li><a href="forms.html">Form advice</a></li> -->
+						<li><a href="docs/French-Translation-Guide.pdf">French translation guide (PDF)</a></li>
+						<li><a href="https://novascotia.sharepoint.com/sites/Projects/CNSGovWebStrategy">Government Web Strategy</a></li>
+						<li><a href="https://cns.pages.novascotia.ca/patternlab/metadata.html" style="color:#0563c1; text-decoration:underline">How we use metadata</a></li>
+						<li><a href="docs/Online-Writing-Guide-v2.pdf">Online writing guide (PDF)</a></li>
+            <li><a href="docs/NS-Visual-Identity-Guide-2020.pdf">Partner Brand Guide (PDF)</a>
+						<li><a href="https://novascotia.sharepoint.com/sites/Projects/CNSGovWebStrategy/Pages/URLs-and-web-addresses.aspx">URLs and web addresses</a></li>
+						<li><a href="docs/Visual-Aids-Guide.pdf">Visual Aids Guide</a></li>
+					</ul>
+        </div>
+    </article>
+    <article class="card">
+       <div class="card-body">
+        <h2>Core brand elements</h2>
+        <p>
+          A consistent and professional brand presence for the Government of Nova Scotia makes it easier for citizens to get the information they need, in a voice they understand and trust.
+        </p>
+        <ul>
+          <li><a href="colours.html">Colours</a></li>
+          <li><a href="logos.html">Logos</a></li>
+          <li><a href="typography.html">Typography</a></li>
+        </ul>
+
+        </div>
+    </article>
+    
+</section>
+
+
+    </div>
+  </div>
+</div>
 
 <?php
 include 'inc/footer.php';
